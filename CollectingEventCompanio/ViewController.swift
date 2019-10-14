d40196564bf34d330920babbfebf3c6ff188329f
Warren Brown <protodrone@gmail.com>
2019-10-13 20:08:19 -0400

    Initial Commit

---
+++ CollectingEventCompanio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CollectingEventCompanio
+//
+//  Created by Warren Brown on 10/13/19.
+//  Copyright © 2019 Warren H Brown. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
